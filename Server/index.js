33f9bb1599490c142197c5d8d9daeb3dd13b6d81
Abhay jindal <abhayjindal8166@gmail.com>
2023-09-18 09:56:56 +0530

    added new routes

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
+import hotelRouter from "./routes/hotels.js";
+import roomRouter from "./routes/rooms.js";
+import userRouter from "./routes/users.js";
 const app = express();      
 dotenv.config();
 
+const DB = process.env.DATABASE.replace('<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+const connect = async () => {
+try{
+    await mongoose.connect(DB);
+    console.log("Successfully connected to MongoDB");
+} catch(error){
+    console.log(error);
+}
+};
+
+// middlewares
+
+app.use(express.json());
+app.use("/auth")
 
 app.listen(8000, () => {
+    connect();
     console.log("Server is running on port 8000.");
 });
